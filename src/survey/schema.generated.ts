ad16f7907d98eaeee4f6d6e029cadfc686a42755
leighannatealdawson <leighannatealdawson@hotmail.com>
2026-06-09 12:09:44 +0100

    Update schema.generated.ts

--- src/survey/schema.generated.ts
+++ src/survey/schema.generated.ts
@@ -387,6 +387,21 @@ export const questions: Question[] = [
     prompt: "If you have any additional comments, feedback or notes please leave them here.",
     required: false,
     multiline: true,
+  },
+  {
+    id: "job",
+    kind: "single",
+    prompt: "Which of the following best describes the industry you currently work in?",
+    required: false,
+    choices: [{"value":"agri","label":"Agriculture, Forestry & Fishing"},{"value":"manuf","label":"Manufacturing"},{"value":"constr","label":"Construction"},{"value":"retail","label":"Wholesale & Retail Trade"},{"value":"transp","label":"Transportation & Storage"},{"value":"info","label":"Information & Communication"},{"value":"finance","label":"Finance & Insurance"},{"value":"prof","label":"Professional, Scientific & Technical"},{"value":"educ","label":"Education"},{"value":"health","label":"Health & Social Work"},{"value":"public","label":"Public Administration"},{"value":"arts","label":"Arts, Entertainment & Recreation"},{"value":"accom","label":"Accommodation & Food Service"},{"value":"other","label":"Other"},{"value":"pnts","label":"Prefer not to say"}],
+    layout: "horizontal",
+  },
+  {
+    id: "hobbies",
+    kind: "multi",
+    prompt: "Which of the following areas do you have an interest in, either as a hobby, personal activity, or professional involvement? ",
+    required: false,
+    choices: [{"value":"comm","label":"Community or group activities"},{"value":"soc","label":"Socialising with friends or family"},{"value":"arts","label":"Arts, culture or events"},{"value":"relax","label":"Relaxing or quiet time"},{"value":"sport","label":"Exercise or sport"},{"value":"outdoor","label":"Outdoor recreation"},{"value":"country","label":"Countryside pursuits"},{"value":"creative","label":"Creative hobbies"},{"value":"digital","label":"Digital entertainment"},{"value":"eatout","label":"Eating out"},{"value":"other","label":"Other"}],
   }
 ];
 
@@ -464,5 +479,7 @@ export const sheetOrderedIds: string[] = [
   "country",
   "postcode",
   "eircode",
-  "comments"
+  "comments",
+  "job",
+  "hobbies"
 ];
